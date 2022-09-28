0cd7bb0df04af2eccac2637b688d5fd610c0c15b
espeandr <espena_92@hotmail.com>
2022-09-28 13:49:07 +0200

    Ensure offset (#4)
    Co-authored-by: Espen Andreassen <espen.andreassen@statnett.no>

--- src/test/java/camel/BreakOnFirstErrorTest.java
+++ src/test/java/camel/BreakOnFirstErrorTest.java
@@ -9,15 +9,18 @@ import org.apache.camel.builder.endpoint.dsl.DirectEndpointBuilderFactory.Direct
 import org.apache.camel.builder.endpoint.dsl.KafkaEndpointBuilderFactory.KafkaEndpointBuilder;
 import org.apache.camel.test.spring.junit5.CamelSpringBootTest;
 import org.apache.camel.test.spring.junit5.UseAdviceWith;
+import org.apache.kafka.clients.consumer.OffsetAndMetadata;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.kafka.test.utils.KafkaTestUtils;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.apache.camel.builder.endpoint.StaticEndpointBuilders.direct;
 import static org.apache.camel.builder.endpoint.StaticEndpointBuilders.kafka;
@@ -30,15 +33,18 @@ import static org.awaitility.Awaitility.await;
 @EmbeddedKafka(controlledShutdown = true, partitions = 1)
 public class BreakOnFirstErrorTest {
 
-    private final List<String> consumedRecords = new ArrayList<>();
 
     @Autowired
     protected CamelContext camelContext;
 
-    @Value("${spring.embedded.kafka.brokers}")
-    private String brokerAddresses;
     @Autowired
     protected ProducerTemplate kafkaProducer;
+
+    private final String kafkaGroupId = "test_group_id";
+    private final String kafkaTopicName = "test_topic";
+    private final List<String> consumedRecords = new ArrayList<>();
+    @Value("${spring.embedded.kafka.brokers}")
+    private String kafkaBrokerAddress;
     private int consumptionCounter = 0;
 
     @BeforeEach
@@ -52,18 +58,42 @@ public class BreakOnFirstErrorTest {
 
     @Test
     public void shouldOnlyReconsumeFailedMessageOnError() {
-        final List<String> producedRecords = List.of("1", "2", "3", "4", "5", "6"); // <- Error is thrown once on record "5"
-        final List<String> expectedConsumedRecords = List.of("1", "2", "3", "4", "5", "5", "6"); // 5 should be consumed twice as error is thrown
+        final List<String> producedRecords = List.of("1", "2", "3", "4", "5", "6", "7"); // <- Error is thrown once on record "5"
+        final List<String> expectedConsumedRecords = List.of("1", "2", "3", "4", "5", "5", "6", "7"); // 5 should be consumed twice as error is thrown
 
-        producedRecords.forEach(kafkaProducer::sendBody);
+        produceRecords(producedRecords);
 
         await().untilAsserted(() ->
                                   // Assertion fails as all records on topic are reconsumed on error.
                                   assertThat(consumedRecords).isEqualTo(expectedConsumedRecords));
     }
 
+    private void produceRecords(final List<String> producedRecords) {
+        // Producing in two batches to ensure application has a committed offset
+        final int size = producedRecords.size();
+        final int middleIndex = (size + 1) / 2;
+        final List<String> firstHalf = new ArrayList<>(producedRecords.subList(0, middleIndex));
+        final List<String> secondHalf = new ArrayList<>(producedRecords.subList(middleIndex, size));
+
+        firstHalf.forEach(kafkaProducer::sendBody);
+        await().until(() -> getCurrentOffset() > 0);
+        System.out.println("Offset committed: " + getCurrentOffset());
+        secondHalf.forEach(kafkaProducer::sendBody);
+    }
+
+    private Long getCurrentOffset() {
+        try {
+            return Optional.ofNullable(KafkaTestUtils.getCurrentOffset(kafkaBrokerAddress, kafkaGroupId, kafkaTopicName, 0))
+                .map(OffsetAndMetadata::offset)
+                .orElse(0L);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     private void createConsumerRoute(RouteBuilder builder) {
         builder.from(kafkaTestTopic()
+                         .groupId(kafkaGroupId)
                          .autoOffsetReset("earliest")
                          .breakOnFirstError(true)
                          .maxPollRecords(4) // 1, 2, 4 causes the test to fail.
@@ -87,7 +117,7 @@ public class BreakOnFirstErrorTest {
     }
 
     private KafkaEndpointBuilder kafkaTestTopic() {
-        return kafka("test_topic")
-            .brokers(brokerAddresses);
+        return kafka(kafkaTopicName)
+            .brokers(kafkaBrokerAddress);
     }
 }
